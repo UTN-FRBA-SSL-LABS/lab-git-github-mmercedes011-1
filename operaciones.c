df2dfda40c689e5bdbd973de5b8cb8af1235043e
mmercedes011 <mninacolque@frba.utn.edu.ar>
2026-05-03 23:48:25 -0300

    wip: experimento roto

--- operaciones.c
+++ operaciones.c
@@ -14,7 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
-int multiplicar(int a, int b) {
+int multiplicar(const int a, const int b) {
     return a * b;
 }
 
@@ -22,4 +22,7 @@ int multiplicar(int a, int b) {
 
 int esPar(int n) {
     return (n % 2) == 0;
+}
+int dividir(int a, int b) {
+    return a - b; /* bug intencional */
 }
